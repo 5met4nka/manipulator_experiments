7d2e1fe5e3ef84c7da0de102e5b3a0c227075973
5met4nka <nlobazev@gmail.com>
2024-01-12 21:21:17 +0300

    add moveit install

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Проверка наличия папки third_party
+# Проверка запуска из папки scripts
 if [ "$(basename "$(pwd)")" != "scripts" ]; then
     echo "Error: Please execute the script from the 'scripts' folder"
     exit 1
@@ -15,3 +15,6 @@ git -C ../third_party clone https://github.com/ros-industrial/motoman.git -b kin
 # abb
 git -C ../third_party clone https://github.com/ros-industrial/abb_experimental.git -b kinetic-devel
 git -C ../third_party clone https://github.com/ros-industrial/abb.git -b kinetic-devel
+
+sudo apt-get update && sudo apt-get install -y \
+    ros-$ROS_DISTRO-moveit
