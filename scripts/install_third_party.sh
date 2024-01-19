0192eee8c042b3b137b3921a8183e1758ca10644
5met4nka <nlobazev@gmail.com>
2024-01-19 16:07:31 +0300

    added experimental packages

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -8,9 +8,11 @@ fi
 
 # fanuc
 git -C ../third_party clone https://github.com/ros-industrial/fanuc.git -b melodic-devel
+git -C ../third_party clone https://github.com/ros-industrial/fanuc_experimental -b melodic-devel
 
 #yaskawa
 git -C ../third_party clone https://github.com/ros-industrial/motoman.git -b kinetic-devel
+git -C ../third_party clone https://github.com/ros-industrial/motoman_experimental -b kinetic-devel
 
 # abb
 git -C ../third_party clone https://github.com/ros-industrial/abb_experimental.git -b kinetic-devel
