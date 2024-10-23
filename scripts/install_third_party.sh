171cedbe8eee259c115e943fafb78257dfbbb8ef
Nikita Lobazev <nlobazev@gmail.com>
2024-10-23 13:45:31 +0300

    added pkgs from moveit tutorial

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -18,3 +18,7 @@ git -C ../third_party clone https://github.com/ros-industrial/motoman_experiment
 git -C ../third_party clone https://github.com/ros-industrial/abb_experimental.git -b kinetic-devel
 git -C ../third_party clone https://github.com/ros-industrial/abb.git -b kinetic-devel
 git -C ../third_party clone https://github.com/ros-industrial/abb_driver.git -b melodic-devel
+
+# moveit tutorials (source https://docs.ros.org/en/melodic/api/moveit_tutorials/html/doc/getting_started/getting_started.html)
+git -C ../third_party clone https://github.com/ros-planning/moveit_tutorials.git -b melodic-devel
+git -C ../third_party clone https://github.com/ros-planning/panda_moveit_config.git -b noetic-devel
